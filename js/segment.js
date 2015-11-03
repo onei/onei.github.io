671f1fad228d81fef5685a9b716a8aab13e72af6
onei <mdowdell244@gmail.com>
2015-11-03 08:43:51 +0000

    almost finished js, tidied up scss, and more I can't remember

--- js/segment.js
+++ js/segment.js
@@ -1,316 +1,495 @@
 
-// @todo disable chevrons during transition
+// @todo add support for reversing order of segments in .letters to options
+// swipe events for mobile support
+// <http://www.javascriptkit.com/javatutors/touchevents2.shtml>
 
 (function ($) {
 
 	'use strict';
 
-	var ids = ['DP', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+		/**
+		 * Boolean to prevent multiple movements being fired until the previous movement has completed.
+		 */
+	var disabled = false,
 
-	/**
-	 * Moves the segments one place to the right
-	 */
-	function moveSegmentsLeft() {
-		var $segments = $('.segment-rail > .segment-display'),
-			$view,
-			i,
-			viewClass,
-			$chevron;
+		/**
+		 * replacement for x*
+		 * @todo document
+		 */
+		x = {
+			/**
+			 * The width of an SVG segment plus the separating margin in pixels
+			 * Used to calculate where to position segments to after movements and during initial positioning
+			 *
+			 * Dynamically calculated based on available screen width
+			 */
+			offset: null,
 
-		$chevron = $('.segment-overlay-left .chevron');
+			/**
+			 * The start of the view area of the rail in pixels
+			 * Used to calculate where to position segments during initial positioning
+			 *
+			 * Dynamically calculated based on available screen width
+			 */
+			start: null,
 
-		if ($chevron.hasClass('disabled')) {
-			console.log('dsiabled');
-			return;
-		}
+			/**
+			 *
+			 */
+			first: null,
 
-		$chevron.addClass('disabled');
+			/**
+			 *
+			 */
+			last: null
+		},
 
-		setTimeout(function () {
-			$chevron.removeClass('disabled');
-		// use just over half a second (the transition time)
-		}, 550);
+		/**
+		 * @todo
+		 */
+		maxElements = 4,
 
-		$segments.each(function () {
-			var $this = $(this),
-				transform = $this.css('transform'),
-				re = /matrix\(\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*(-?\d+),\s*(-?\d+)\s*\)/,
-				matches = transform.match(re),
-				css,
-				x,
-				y;
+		/**
+		 * @todo
+		 */
+		init = {
+			/**
+			 * @todo
+			 */
+			main: function () {
+				// calculate `x.offset` and `x.start`
+				init.setDimensions();
 
-			if (matches === null) {
-				// sometimes happens when the cehvrons are clicked too quickly
-				// except it's the opposite direction the the chevron that's has the error
-				// in this case right
-				return;
+				// set initial segment positions
+				init.setupSegments();
+
+				// set initial highlight colour
+				init.setColour();
+
+				// segment click handlers
+				$('#segRail .seg').click(segments.highlight);
+
+				// chevron click handlers
+				$('#chevronLeft').click(segments.moveLeft);
+				$('#chevronRight').click(segments.moveRight);
+
+				// colour pick click handlers
+				$('#optionsColours .pick').click(options.colourPick);
+
+				// letters click handlers
+				$('#inputsLetters .letter').click(inputs.highlightLetter);
+
+			},
+
+			/**
+			 * Sets `x.start` and `x.offset` which are used for positioning each segment
+			 * These are set dynamically to allow for greater flexibility with variable screen width
+			 */
+			setDimensions: function () {
+				var maxWidth = $('#article').width(),
+					// just check the left one, as they're both the same
+					overlayWidth = $('#segOverlayLeft').width(),
+					segmentWidth = $('#segRail .seg-display').width(),
+					separatorWidth = (maxWidth - (2 * overlayWidth) - (segmentWidth * 4)) / 3;
+
+				x.offset = segmentWidth + separatorWidth;
+				x.start = overlayWidth;
+
+				console.log('xstart, xOffset:', x.start, x.offset);
+			},
+
+			/**
+			 * Set initial positioning for each segment and adds data-view attributes to the editable segments
+			 */
+			setupSegments: function () {
+				var $segs = $('#segRail .seg-display');
+
+				$segs.each(function (index) {
+					var $this = $(this),
+						viewIndex = index - 2,
+						xPosition = x.start + (viewIndex * x.offset),
+						// @todo use some sort of string format method?
+						css = 'translateX(' + xPosition + 'px)';
+
+					$this.css('transform', css);
+
+					if (viewIndex >= 0 && viewIndex < 4) {
+						$this.attr('data-view', viewIndex);
+					}
+
+					if (index === 0) {
+						x.first = xPosition;
+					} else if (index === 7) {
+						x.last = xPosition;
+					}
+				});
+			},
+
+			/**
+			 * Sets green as the initial highlight colour
+			 */
+			setColour: function () {
+				$('#coloursPickGreen').addClass('selected');
+				$('#segRail, #inputsLetters').addClass('green');
 			}
+		},
 
-			x = matches[1] * 1;
-			y = matches[2];
+		segments = {
+			/**
+			 * Moves the segments one place left on the rail.
+			 */
+			moveLeft: function () {
+				// don't allow more than one movement to be submitted at a time
+				if (disabled) {
+					return;
+				}
 
-			x -= 134;
-			x += 'px';
-			y += 'px';
+				disabled = true;
 
-			css = 'translate(' + x + ', ' + y + ')';
-			$this.css('transform', css);
-		});
+				// allow another movement when the animation has completed (500ms)
+				setTimeout(function () {
+					disabled = false;
+				}, 500);
 
-		$view = $segments.filter('.view-0').next();
+				var $segs = $('#segRail .seg-display'),
+					re = /matrix\(\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*(-?\d+),\s*(-?\d+)\s*\)/,
+					viewFirst;
 
-		for (i = 0; i < 4; i++) {
-			viewClass = 'view-' + i;
+				$segs.each(function () {
+					var $this = $(this),
+						transform = $this.css('transform'),
+						matches = transform.match(re),
+						xPosition,
+						css;
 
-			// remove the class from the old view element
-			$segments.removeClass(viewClass);
-			// add it to the new one
-			$view.addClass(viewClass);
-			// and move to the next viewed element
-			$view = $view.next();
-		}
+					if (matches === null) {
+						console.log('Error: No matches found.');
+						return;
+					}
 
-		appendSegment();
-		updateInputs();
-	}
+					xPosition = matches[1] * 1;
+					xPosition -= x.offset;
 
-	/**
-	 * Moves the segments one place to the left
-	 */
-	function moveSegmentsRight() {
-		var $segments = $('.segment-rail > .segment-display'),
-			$view,
-			i,
-			viewClass,
-			$chevron;
+					css = 'translateX(' + xPosition + 'px)';
+					$this.css('transform', css);
+				});
 
-		$chevron = $('.segment-overlay-right .chevron');
+				// get the index of the previous first item in the view
+				viewFirst = $segs.filter('[data-view="0"]').attr('data-index');
+				// convert to number and move it forward one
+				viewFirst = (viewFirst * 1) + 1;
 
-		if ($chevron.hasClass('disabled')) {
-			console.log('dsiabled');
-			return;
-		}
+				segments.reView(viewFirst);
+				segments.append();
+				inputs.rebuildHighlights(viewFirst);
+			},
 
-		$chevron.addClass('disabled');
+			/**
+			 * Moves the segments one place right on the rail.
+			 */
+			moveRight: function () {
+				// don't allow more than one movement to be submitted at a time
+				if (disabled) {
+					return;
+				}
 
-		setTimeout(function () {
-			$chevron.removeClass('disabled');
-		// use just over half a second (the transition time)
-		}, 550);
+				disabled = true;
 
-		// move each segment 1 place right
-		$segments.each(function () {
-			var $this = $(this),
-				transform = $this.css('transform'),
-				re = /matrix\(\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*(-?\d+),\s*(-?\d+)\s*\)/,
-				matches = transform.match(re),
-				css,
-				x,
-				y;
+				// allow another movement when the animation has completed (500ms)
+				setTimeout(function () {
+					disabled = false;
+				}, 500);
 
-			if (matches === null) {
-				// sometimes happens when the cehvrons are clicked too quickly
-				// except it's the opposite direction the the chevron that's has the error
-				// in this case left
-				return;
+				var $segs = $('#segRail .seg-display'),
+					re = /matrix\(\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*-?\d+,\s*(-?\d+),\s*(-?\d+)\s*\)/,
+					viewFirst;
+
+				$segs.each(function () {
+					var $this = $(this),
+						transform = $this.css('transform'),
+						matches = transform.match(re),
+						xPosition,
+						css;
+
+					if (matches === null) {
+						console.log('Error: No matches found.');
+						return;
+					}
+
+					xPosition = matches[1] * 1;
+					xPosition += x.offset;
+
+					css = 'translateX(' + xPosition + 'px)';
+					$this.css('transform', css);
+				});
+
+				// get the index of the previous first item in the view
+				viewFirst = $segs.filter('[data-view="0"]').attr('data-index');
+				// convert to number and move it forward one
+				viewFirst = (viewFirst * 1) - 1;
+
+				console.log(viewFirst);
+
+				segments.reView(viewFirst);
+				segments.prepend();
+
+				// fix for when prepending an element causes the indexing to be incorrect
+				if ($segs.length < $('#segRail .seg-display').length) {
+					console.log('extra element detected');
+					viewFirst += 1
+				}
+
+				inputs.rebuildHighlights(viewFirst);
+			},
+
+			/**
+			 * Appends a segment to the rail.
+			 */
+			append: function () {
+				var $rail = $('#segRail'),
+					$segs = $rail.find('.seg-display'),
+					$lastViewSeg = $segs.filter('[data-view="3"]'),
+					index = $lastViewSeg.attr('data-index') * 1,
+					$clone,
+					css;
+
+				// make sure there's a minimum of 2 after the view area
+				if (index + 2 >= $segs.length) {
+					// clone click handlers on the svg paths
+					$clone = $lastViewSeg.clone(true);
+
+					css = 'translateX(' + x.last + 'px)';
+					$clone.css('transform', css);
+
+					$clone.removeAttr('data-view');
+
+					$rail.append($clone);
+					segments.reIndex();
+				}
+			},
+
+			/**
+			 * Prepends a segment to the rail.
+			 */
+			prepend: function () {
+				var $rail = $('#segRail'),
+					$firstViewSeg = $rail.find('.seg-display[data-view="0"]'),
+					index = $firstViewSeg.attr('data-index') * 1,
+					$clone,
+					css;
+
+				// make sure there's a minimum of 2 before the view area
+				if (index - 2 < 0) {
+					// clone click handlers on the svg paths
+					$clone = $firstViewSeg.clone(true);
+
+					css = 'translateX(' + x.first + 'px)';
+					$clone.css('transform', css);
+
+					$clone.removeAttr('data-view');
+
+					$rail.prepend($clone);
+					segments.reIndex();
+				}
+			},
+
+			/**
+			 * Re-indexes each segment following a segment being added.
+			 */
+			reIndex: function () {
+				var $segs = $('#segRail .seg-display');
+
+				$segs.removeAttr('data-index');
+
+				$segs.each(function (index) {
+					$(this).attr('data-index', index);
+				});
+			},
+
+			/**
+			 * Re-calculate view indexes.
+			 */
+			reView: function (index) {
+				var $segs = $('#segRail .seg-display'),
+					$oldView,
+					i;
+
+				$segs.removeAttr('data-view');
+
+				$segs.slice(index, index + 4).each(function (i) {
+					$(this).attr('data-view', i);
+				});
+			},
+
+
+			/**
+			 * Adds or removes a segment's highlight.
+			 */
+			highlight: function () {
+				var $this = $(this),
+					$view = $this.parents('.seg-display'),
+					letter = $this.attr('data-segment'),
+					viewIndex = $view.attr('data-view'),
+					$letter = $('#inputsLetters .view')
+						// filter to the group of letters associated with the correct view index
+						.filter('[data-view="' + viewIndex + '"]')
+						.find('.letter')
+						// filter to the correct letter
+						.filter('[data-letter="' + letter + '"]');
+
+				if (svg.hasClass($this, 'on')) {
+					svg.removeClass($this, 'on');
+					$letter.removeClass('on');
+				} else {
+					svg.addClass($this, 'on');
+					$letter.addClass('on');
+				}
 			}
+		},
 
-			x = matches[1] * 1;
-			y = matches[2];
+		/**
+		 * Methods relting to the options section
+		 */
+		options = {
+			/**
+			 * Click handler for colour picker
+			 */
+			colourPick: function () {
+				var $this = $(this),
+					$old = $('#optionsColours .selected'),
+					$update = $('#segRail, #inputsLetters'),
+					newClass = $this.attr('data-colour'),
+					oldClass = $old.attr('data-colour');
 
-			// @todo define this dynamically (mobile)
-			x += 134;
-			x += 'px';
-			y += 'px';
+				// move selected class
+				$old.removeClass('selected');
+				$this.addClass('selected');
 
-			css = 'translate(' + x + ', ' + y + ')';
-			$this.css('transform', css);
-		});
-
-		$view = $segments.filter('.view-0').prev();
-
-		for (i = 0; i < 4; i++) {
-			viewClass = 'view-' + i;
-
-			// remove the class from the old view element
-			$segments.removeClass(viewClass);
-			// add it to the new one
-			$view.addClass(viewClass);
-			// and move to the next viewed element
-			$view = $view.next();
-		}
-
-		prependSegment();
-		updateInputs();
-	}
-
-	/**
-	 * Prepends a new segment to the segment rail
-	 */
-	function prependSegment() {
-		var $rail = $('.segment-rail'),
-			$view = $rail.find('.view-0'),
-			index = getSegIndex($view),
-			$clone;
-
-		if (index >= 2) {
-			return;
-		}
-
-		// clone events
-		$clone = $view.clone(true);
-		$clone.removeClass('view-0');
-		$clone.css('transform', 'translate(-44px, 5px)');
-		$rail.prepend($clone);
-
-		reIndex();
-	}
-
-	/**
-	 * Appends a new segment to the segment rail
-	 */
-	function appendSegment() {
-		var $rail = $('.segment-rail'),
-			$view = $rail.find('.view-3'),
-			index = getSegIndex($view),
-			$clone;
-
-		if ((index + 3) <= $rail.find('.segment-display').length) {
-			return;
-		}
-
-		// clone events
-		$clone = $view.clone(true);
-		$clone.removeClass('view-3');
-		$clone.css('transform', 'translate(894px, 5px)');
-		$rail.append($clone);
-
-		reIndex();
-	}
-
-	/**
-	 * Re-indexes all the segments following a asegment being appended or prepended to the rail
-	 */
-	function reIndex() {
-		var $segments = $('.segment-rail > .segment-display');
-
-		$segments.each(function (index) {
-			var $this = $(this),
-				oldIndex = getSegIndex($this),
-				newIndex = 'index-' + index;
-
-			if (oldIndex !== false) {
-				oldIndex = 'index-' + oldIndex;
-				$this.removeClass(oldIndex);
+				// swap class on rail and letters
+				$update.removeClass(oldClass).addClass(newClass);
 			}
+		},
 
-			$this.addClass(newIndex);
-		});
-	}
+		/**
+		 * Methods relating to the inputs section
+		 */
+		inputs = {
+			/**
+			 * Add or remove a highlight to a letter.
+			 */
+			highlightLetter: function () {
+				var $this = $(this),
+					$view = $this.parent('.view'),
+					letter = $this.attr('data-letter'),
+					viewIndex = $view.attr('data-view'),
+					$segment = $('#segRail .seg-display')
+						.filter('[data-view="' + viewIndex + '"]')
+						.find('.seg')
+						.filter('[data-segment="' + letter + '"]');
 
-	function updateInputs() {}
+				if ($this.hasClass('on')) {
+					$this.removeClass('on');
+					svg.removeClass($segment, 'on');
+				} else {
+					$this.addClass('on');
+					svg.addClass($segment, 'on');
+				}
+			},
 
-	/**
-	 * Gets the segment's index number for comparison
-	 */
-	function getSegIndex($segment) {
-			// use native js thing for this
-		var segClass = ' ' + $segment.attr('class') + ' ',
-			re = /\sindex-(\d+)\s/,
-			index,
-			matches;
+			/**
+			 * Rebuild the highlighting for letters (and inputs?) after the segments are moved on the rail.
+			 */
+			rebuildHighlights: function (index) {
+				var ids = ['DP', 'G', 'F', 'E', 'D', 'C', 'B', 'A'],
+					$letters = $('#inputsLetters .view'),
+					$segs = $('#segRail .seg-display').slice(index, index + 4);
 
-		matches = segClass.match(re);
+				console.log($segs.get());
+				console.log($segs.get(0));
 
-		if (matches === null) {
-			return false;
-		}
+				$segs.each(function (i) {
+					var $seg = $(this),
+						$letter = $letters.eq(i).find('.letter');
 
-		index = matches[1] * 1;
-		return index;
-	}
+					console.log($seg.get(0), $letter.get(0));
 
-	function getSegConfig($segment) {
-		var $segs = $segment.find('.seg'),
-			config = {},
-			binary = '';
+					// start with a clean slate
+					$letter.removeClass('on');
 
-		ids.forEach(function (elem) {
-			var segClass = '.seg-' + elem,
-				$seg = $segs.filter(segClass).first(),
-				hasClass = $seg.hasClass('on');
+					ids.forEach(function (id) {
+						var $path = $seg.find('[data-segment="' + id + '"]');
 
-			config[elem] = (hasClass ? true : false);
-			binary += (hasClass ? '1' : '0');
-		});
+						if (svg.hasClass($path, 'on')) {
+							$letter.filter('[data-letter="' + id + '"]').addClass('on');
+						}
+					});
+				});
+			}
+		},
 
-		config.binary = binary;
+		/**
+		 * Methods for manipulating SVG class attributes
+		 */
+		svg = {
+			/**
+			 * Add a class to an SVG element.
+			 *
+			 * @param $elem {jQuery.object}
+			 * @param toAdd {String}
+			 */
+			addClass: function ($elem, toAdd) {
+				// don't duplicate classes
+				if (svg.hasClass($elem, toAdd)) {
+					return;
+				}
 
-		// first, convert `binary` valid binary string
-		// then convert it number (decimal)
-		// finally convert to hexidecimal (base 16)
-		config.hex = (('0b' + binary) * 1).toString(16);
+				var classes = $elem.attr('class');
+				classes += ' ' + toAdd;
+				$elem.attr('class', classes);
+			},
 
-		return config;
-	}
+			/**
+			 * Remove a class from a SVG element.
+			 *
+			 * @param $elem {jQuery.object}
+			 * @param toAdd {String}
+			 */
+			removeClass: function ($elem, toRemove) {
+				toRemove = toRemove.trim();
 
-	/**
-	 * Adds support for removing classes from elements in svg
-	 */
-	function svgRemoveClass($elem, toRemove) {
-		var classes = $elem.attr('class').split(/\s+/),
-			index = classes.indexOf(toRemove);
+				var classes = $elem.attr('class').split(/\s/),
+					index = classes.indexOf(toRemove);
 
-		if (index > -1) {
-			classes.splice(index, 1);
-		}
+				if (index > -1) {
+					classes.splice(index, 1);
+					classes = classes.join(' ');
+					$elem.attr('class', classes);
 
-		classes = classes.join(' ');
-		$elem.attr('class', classes);
-	}
+				}
+			},
 
-	/**
-	 * Adds support for adding classes to elements in svg
-	 */
-	function svgAddClass($elem, toAdd) {
-		var classes = $elem.attr('class');
+			/**
+			 * Test if an SVG element has a specified class.
+			 *
+			 * @param $elem {jQuery.object}
+			 * @param toAdd {String}
+			 *
+			 * @returns {Boolean}
+			 */
+			hasClass: function ($elem, toCheck) {
+				var classes = $elem.attr('class');
 
-		classes += ' ' + toAdd;
-		$elem.attr('class', classes);
-	}
+				classes = ' ' + classes + ' ';
+				toCheck = ' ' + toCheck + ' ';
 
-	/**
-	 * Adds support for checking if an svg element has a specified class
-	 */
-	function svgHasClass($elem, toCheck) {
-		var classes = $elem.attr('class').split(/\s+/);
+				if (classes.indexOf(toCheck) > -1) {
+					return true;
+				}
 
-		if (classes.indexOf(toCheck) > -1) {
-			return true;
-		}
+				return false;
+			}
+		};
 
-		return false;
-	}
-
-	function segHighlight() {
-		var $this = $(this),
-			func = svgHasClass($this, 'on') ?
-				svgRemoveClass :
-				svgAddClass;
-
-		func($this, 'on');
-	}
-
-	function init() {
-		// segment click handlers
-		$('.segment-7 .seg').click(segHighlight);
-
-		// chevron click handlers
-		$('.segment-overlay-left .chevron').click(moveSegmentsLeft);
-		$('.segment-overlay-right .chevron').click(moveSegmentsRight);
-	}
-
-	$(init);
+	$(init.main);
 
 }(jQuery));
